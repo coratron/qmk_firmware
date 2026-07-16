fed63f94ac300742c35235dc92fd042413b040d0
Gabriel Ballesteros <gabriel@coratronics.com>
2026-07-16 12:05:39 +1000

    coratron: retain left-hand positions on KiCad layer

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/coratron/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/coratron/keymap.c
@@ -13,20 +13,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_5x7(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_PIPE, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_BSLS, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LGUI, LGUI(KC_E), KC_SPC, LT(3, KC_ENT), TT(1), KC_BSPC, KC_LCTL, KC_GRV, KC_LALT, KC_QUOT, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_PPLS, TG(2), KC_QUES, KC_Y, KC_U, KC_I, KC_O, KC_P, LCTL(LALT(KC_B)), KC_SLSH, KC_H, KC_J, KC_K, KC_L, KC_PGUP, LCTL(LALT(KC_U)), KC_N, KC_M, KC_COMM, KC_DOT, KC_PGDN, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_SPC, KC_ENT, KC_DEL, KC_SCLN, KC_COLN, KC_EQL),
     [1] = LAYOUT_5x7(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_LPRN, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_F7, KC_LCBR, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, QK_BOOT, KC_LBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_TRNS, KC_TRNS, LCTL(LALT(KC_MINUS)), KC_UP, LCTL(LSFT(KC_MINUS)), KC_TRNS, KC_RPRN, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_RCBR, KC_TRNS, KC_HOME, KC_TRNS, KC_END, KC_TRNS, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_LPRN, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS, KC_LCBR, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, QK_BOOT, KC_LBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_TRNS, KC_TRNS, LCTL(LALT(KC_MINUS)), KC_UP, LCTL(LSFT(KC_MINUS)), KC_TRNS, KC_RPRN, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_RCBR, KC_TRNS, KC_HOME, KC_TRNS, KC_END, KC_TRNS, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [2] = LAYOUT_5x7(
         KC_TRNS, KC_TRNS, KC_MSTP, KC_MRWD, KC_MFFD, KC_MPLY, KC_STOP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CALC, KC_NO, KC_NLCK, KC_TRNS, KC_PMNS, KC_PPLS, KC_TRNS, KC_NO, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_P4, KC_P5, KC_P6, KC_PAST, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_PSLS, KC_TRNS, KC_P0, KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    // Hold left Enter to access KiCad actions without using the right half.
-    // Each position emits the KiCad: Unlocked QWERTY shortcut for the labelled action.
+    // Hold left Enter for KiCad while retaining the normal left-hand number and letter positions.
     [3] = LAYOUT_5x7(
-        KC_ESC,          KC_1,        KC_GRV,      KC_W,        KC_Q,        KC_S,        KC_D,
-        KC_TAB,          KC_R,        KC_Y,        KC_P,        KC_J,        KC_K,        KC_N,
-        KC_CAPS,         KC_A,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,
-        OSM(MOD_LSFT),   KC_E,        KC_C,        KC_Z,        KC_X,        KC_G,
-        KC_BSPC,         KC_T,        KC_U,        S(KC_M),
-                                                  KC_SPC,      KC_F4,
-                                                  KC_F6,       KC_F7,
-                                                  LCTL(KC_S),  LCTL(KC_Y),
+        KC_ESC,          KC_1,            KC_2,            KC_3,            KC_4,              KC_5,              KC_6,
+        KC_TAB,          KC_Q,            KC_W,            KC_E,            KC_R,              KC_T,              KC_M,
+        KC_CAPS,         KC_A,            KC_S,            KC_D,            KC_F,              KC_G,              LCTL(KC_D),
+        OSM(MOD_LSFT),   KC_Z,            KC_X,            KC_C,            KC_V,              KC_B,
+        KC_P,            KC_L,            KC_SPC,          KC_ENT,
+                                                      KC_H,              KC_BSPC,
+                                                      KC_LCTL,           KC_GRV,
+                                                      KC_J,              LCTL(KC_Y),
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
